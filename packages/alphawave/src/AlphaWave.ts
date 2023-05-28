760791e18fa6d46e7d72ab87748a9387edbcf52a
Steven Ickman <stevenic@microsoft.com>
2023-05-27 23:03:19 -0700

    Fleshed out conversation repair logic.

--- packages/alphawave/src/AlphaWave.ts
+++ packages/alphawave/src/AlphaWave.ts
@@ -1,6 +1,7 @@
 import { FunctionRegistry, GPT3Tokenizer, Message, PromptFunctions, PromptMemory, PromptSection, Tokenizer, VolatileMemory } from "promptrix";
-import { PromptCompletionClient, PromptCompletionOptions, PromptResponse, PromptResponseValidator } from "./types";
+import { PromptCompletionClient, PromptCompletionOptions, PromptResponse, PromptResponseValidation, PromptResponseValidator } from "./types";
 import { DefaultResponseValidator } from "./DefaultResponseValidator";
+import { ConversationHistoryMemoryFork } from "./ConversationHistoryMemoryFork";
 
 
 export interface AlphaWaveOptions {
@@ -48,9 +49,8 @@ export class AlphaWave {
         const { client, prompt, prompt_options, functions, memory, tokenizer, validator, max_repair_attempts, history_variable, input_variable } = this.options;
 
         try {
-            // Get user input and add to history
-            const input = memory.get(input_variable);
-            this.addInputToHistory(input, history_variable);
+            // Get user input
+            const input: string = input_variable && memory.has(input_variable) ? memory.get(input_variable) : '';
 
             // Ask client to complete prompt
             const result = await client.completePrompt(prompt, prompt_options);
@@ -66,15 +66,30 @@ export class AlphaWave {
             // Validate response
             const validation = await validator.validateResponse(memory, functions, tokenizer, result);
             if (validation.isValid) {
-                this.addResponseToHistory(result.response, history_variable);
+                // Update history and return
+                this.addInputToHistory(memory, history_variable, input);
+                this.addResponseToHistory(memory, history_variable, result.response);
                 return result;
             }
 
+            // Fork the conversation history and update the fork with the invalid response.
+            const fork = new ConversationHistoryMemoryFork(memory, history_variable, input_variable);
+            this.addInputToHistory(fork, history_variable, input);
+            this.addResponseToHistory(fork, history_variable, result.response);
+
             // Attempt to repair response
-            return {
-                status: 'invalid_response',
-                response: result.response
-            };
+            const repair = await this.repairResponse(fork, functions, tokenizer, validation, max_repair_attempts);
+
+            // Update history with repaired response if successful.
+            // - conversation history will be left unchanged if the repair failed.
+            // - we never want to save an invalid response to conversation history.
+            // - the caller can take further corrective action, including simply re-trying.
+            if (repair.status === 'success') {
+                this.addInputToHistory(memory, history_variable, input);
+                this.addResponseToHistory(memory, history_variable, repair.response as Message);
+            }
+
+            return repair;
         } catch (err: unknown) {
             return {
                 status: 'error',
@@ -83,15 +98,60 @@ export class AlphaWave {
         }
     }
 
-    private addInputToHistory(input: string, variable: string): void {
-        if (input && variable) {
-            const history: Message[] = this.options.memory.get(variable) ?? [];
+    private addInputToHistory(memory: PromptMemory, variable: string, input: string): void {
+        if (variable && input.length > 0) {
+            const history: Message[] = memory.get(variable) ?? [];
             history.push({ role: 'user', content: input });
-            this.options.memory.set(variable, history);
+            memory.set(variable, history);
         }
     }
 
-    private addResponseToHistory(response: Message, variable: string): void {
+    private addResponseToHistory(memory: PromptMemory, variable: string, message: Message): void {
+        if (variable) {
+            const history: Message[] = memory.get(variable) ?? [];
+            history.push(message);
+            memory.set(variable, history);
+        }
+    }
 
+    private async repairResponse(fork: ConversationHistoryMemoryFork, functions: PromptFunctions, tokenizer: Tokenizer, validation: PromptResponseValidation, remaining_attempts: number): Promise<PromptResponse> {
+        const { client, prompt, prompt_options, history_variable, input_variable, validator } = this.options;
+
+        // Are we out of attempts?
+        const feedback = validation.feedback ?? 'The response was invalid. Try another strategy.';
+        if (remaining_attempts <= 0) {
+            return {
+                status: 'invalid_response',
+                response: feedback
+            };
+        }
+
+        // Update the input with the feedback message
+        if (remaining_attempts == 1) {
+            // Tell the model it's the last try so think in steps.
+            fork.set(input_variable, `${feedback} Last try so think step by step.`);
+        } else {
+            fork.set(input_variable, feedback);
+        }
+
+        // Ask client to complete prompt
+        const result = await client.completePrompt(prompt, prompt_options);
+        if (result.status !== 'success') {
+            return result;
+        }
+
+        // Ensure response is a message
+        if (typeof result.response !== 'object') {
+            result.response = { role: 'assistant', content: result.response ?? '' };
+        }
+
+        // Validate response
+        validation = await validator.validateResponse(fork, functions, tokenizer, result);
+        if (validation.isValid) {
+            return result;
+        }
+
+        // Try next attempt
+        return await this.repairResponse(fork, functions, tokenizer, validation, remaining_attempts - 1);
     }
 }
--- packages/alphawave/src/ConversationHistoryMemoryFork.ts
+++ packages/alphawave/src/ConversationHistoryMemoryFork.ts
@@ -1,23 +1,36 @@
 import { Message, PromptMemory } from "promptrix";
 
+/**
+ * A PromptMemory implementation that forks the memory of the current history variable.
+ * @remarks
+ * This creates a temporary in-memory fork of the current conversation history. The current
+ * conversation history is cloned into the fork, and any changes made to the fork will not
+ * affect the original history.
+ */
 export class ConversationHistoryMemoryFork implements PromptMemory {
     private readonly _memory: PromptMemory;
     private readonly _historyVariable: string;
+    private readonly _inputVariable: string;
     private _history: Array<Message>;
+    private _input: string;
 
-    public constructor(memory: PromptMemory, historyVariable: string) {
+    public constructor(memory: PromptMemory, historyVariable: string, inputVariable: string) {
         this._memory = memory;
         this._historyVariable = historyVariable;
-        this._history = memory.has(historyVariable) ? (memory.get(historyVariable) as Array<Message>).slice() : [];
+        this._inputVariable = inputVariable;
+        this._history = historyVariable && memory.has(historyVariable) ? (memory.get(historyVariable) as Array<Message>).slice() : [];
+        this._input = inputVariable && memory.has(inputVariable) ? (memory.get(inputVariable) as string) : '';
     }
 
     public has(key: string): boolean {
-        return (key === this._historyVariable || this._memory.has(key));
+        return (key === this._historyVariable || key === this._inputVariable || this._memory.has(key));
     }
 
     public get<TValue = any>(key: string): TValue {
         if (key === this._historyVariable) {
             return this._history as any;
+        } else if (key === this._inputVariable) {
+            return this._input as any;
         } else {
             return this._memory.get(key);
         }
@@ -26,6 +39,8 @@ export class ConversationHistoryMemoryFork implements PromptMemory {
     public set<TValue = any>(key: string, value: TValue): void {
         if (key === this._historyVariable) {
             this._history = (value as any) ?? [];
+        } else if (key === this._inputVariable) {
+            this._input = (value as any) ?? '';
         } else {
             this._memory.set(key, value);
         }
@@ -34,6 +49,8 @@ export class ConversationHistoryMemoryFork implements PromptMemory {
     public delete(key: string): void {
         if (key === this._historyVariable) {
             this._history = [];
+        } else if (key === this._inputVariable) {
+            this._input = '';
         } else {
             this._memory.delete(key);
         }
@@ -41,6 +58,7 @@ export class ConversationHistoryMemoryFork implements PromptMemory {
 
     public clear(): void {
         this._history = [];
+        this._input = '';
         this._memory.clear();
     }
 }
