376ac93ee369173744f2277d0f36bfead516b5e2
Steven Ickman <stevenic@microsoft.com>
2023-06-01 22:04:04 -0700

    Light bot sample for teams

---
+++ samples/20.teams-lightbot/src/index.ts
@@ -0,0 +1,177 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// Import required packages
+import { config } from 'dotenv';
+import * as path from 'path';
+import * as restify from 'restify';
+
+// Import required bot services.
+// See https://aka.ms/bot-services to learn more about the different parts of a bot.
+import {
+    CloudAdapter,
+    ConfigurationBotFrameworkAuthentication,
+    ConfigurationBotFrameworkAuthenticationOptions,
+    MemoryStorage,
+    TurnContext
+} from 'botbuilder';
+
+// Read botFilePath and botFileSecret from .env file.
+const ENV_FILE = path.join(__dirname, '..', '.env');
+config({ path: ENV_FILE });
+
+const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(
+    process.env as ConfigurationBotFrameworkAuthenticationOptions
+);
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about how bots work.
+const adapter = new CloudAdapter(botFrameworkAuthentication);
+
+// Create storage to use
+//const storage = new MemoryStorage();
+
+// Catch-all for errors.
+const onTurnErrorHandler = async (context: TurnContext, error: Error) => {
+    // This check writes out errors to console log .vs. app insights.
+    // NOTE: In production environment, you should consider logging this to Azure
+    //       application insights.
+    console.error(`\n [onTurnError] unhandled error: ${error.toString()}`);
+
+    // Send a trace activity, which will be displayed in Bot Framework Emulator
+    await context.sendTraceActivity(
+        'OnTurnError Trace',
+        `${error.toString()}`,
+        'https://www.botframework.com/schemas/error',
+        'TurnError'
+    );
+
+    // Send a message to the user
+    await context.sendActivity('The bot encountered an error or bug.');
+    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+};
+
+// Set the onTurnError for the singleton CloudAdapter.
+adapter.onTurnError = onTurnErrorHandler;
+
+// Create HTTP server.
+const server = restify.createServer();
+server.use(restify.plugins.bodyParser());
+
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log(`\n${server.name} listening to ${server.url}`);
+    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+    console.log('\nTo test your bot in Teams, sideload the app manifest.json within Teams Apps.');
+});
+
+import { AI, Application, DefaultPromptManager, DefaultTurnState } from '@microsoft/teams-ai';
+import { OpenAIClient } from "alphawave";
+import { ActionPlanner, PlanValidator } from "alphawave-teams";
+import * as responses from './responses';
+
+interface ConversationState {
+    lightsOn: boolean;
+}
+type ApplicationTurnState = DefaultTurnState<ConversationState>;
+type TData = Record<string, any>;
+
+if (!process.env.OpenAIKey) {
+    throw new Error('Missing environment variables - please check that OpenAIKey is set.');
+}
+
+// Create client
+const client = new OpenAIClient({
+    apiKey: process.env.OpenAIKey,
+    logRequests: true
+});
+
+// Create validator and define actions
+const validator = new PlanValidator();
+validator.action('LightsOn');
+validator.action('LightsOff');
+validator.action('Pause', {
+    type: 'object',
+    properties: {
+        time: {
+            type: 'number'
+        }
+    },
+    required: ['time']
+});
+
+// Create planner
+const planner = new ActionPlanner<ApplicationTurnState>({
+    client,
+    validator,
+    prompt_options: {
+        completion_type: 'chat',
+        model: 'gpt-3.5-turbo'
+    }
+});
+const promptManager = new DefaultPromptManager<ApplicationTurnState>(path.join(__dirname, '../src/prompts'));
+
+// Define storage and application
+const storage = new MemoryStorage();
+const app = new Application<ApplicationTurnState>({
+    storage,
+    ai: {
+        planner,
+        promptManager,
+        prompt: 'chatGPT'
+    }
+});
+
+// Add a prompt function for getting the current status of the lights
+app.ai.prompts.addFunction(
+    'getLightStatus',
+    async (context: TurnContext, state: ApplicationTurnState) => {
+        return state.conversation.value.lightsOn ? 'on' : 'off';
+    }
+);
+
+// Register action handlers
+app.ai.action(
+    'LightsOn',
+    async (context: TurnContext, state: ApplicationTurnState) => {
+        state.conversation.value.lightsOn = true;
+        await context.sendActivity(`[lights on]`);
+        return true;
+    }
+);
+
+app.ai.action(
+    'LightsOff',
+    async (context: TurnContext, state: ApplicationTurnState) => {
+        state.conversation.value.lightsOn = false;
+        await context.sendActivity(`[lights off]`);
+        return true;
+    }
+);
+
+app.ai.action(
+    'Pause',
+    async (context: TurnContext, state: ApplicationTurnState, data: TData) => {
+    const time = data.time ? parseInt(data.time) : 1000;
+        await context.sendActivity(`[pausing for ${time / 1000} seconds]`);
+        await new Promise((resolve) => setTimeout(resolve, time));
+        return true;
+    }
+);
+
+// Register a handler to handle unknown actions that might be predicted
+app.ai.action(
+    AI.UnknownActionName,
+    async (context: TurnContext, state: ApplicationTurnState, data: TData, action: string | undefined) => {
+        await context.sendActivity(responses.unknownAction(action || 'unknown'));
+        return false;
+    }
+);
+
+// Listen for incoming server requests.
+server.post('/api/messages', async (req, res) => {
+    // Route received a request to adapter for processing
+    await adapter.process(req, res as any, async (context) => {
+        // Dispatch to application for routing
+        await app.run(context);
+    });
+});
